b5f802b387df1136104cadd39ea06a837448b765
Olivier Wouters <olivier5741@gmail.com>
2023-02-13 10:36:50 +0100

    dotenv from esm.sh

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as dotenv from "https://deno.land/x/dotenv"
+import * as dotenv from "https://esm.sh/dotenv@16.0.3"
 dotenv.config()
 
 import { App } from "https://deno.land/x/slack_bolt@3.12.2"
